a8280acdf4626a99a97f400395a983f42dace435
IsaiasQB <isaias.qb@hotmail.com>
2022-03-24 03:24:46 -0400

    worked on formating the task area

--- assets/javascript.js
+++ assets/javascript.js
@@ -2,24 +2,34 @@
 var hoursArr = ["7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19"]
 
 // display the local time
-$("#currentDay").text(moment().toString())
+$("#currentDay").text(moment().format("LL").toString())
 
 
 // console.log(time)
+var currentHour = moment().format("H").toString() // this is the current hour 02
 
 // dinamically create time blocks
 for(var i = 0; i < hoursArr.length; i++){
+
+// var hourByBlock = moment().set("hour", hoursArr[i]).format("H").toString()
+
 var hourBlockContainer = $("<div>").addClass("row");
-var hourDisplay = $("<div>").addClass("hour-display col-lg-1 bg-light").text(moment(moment(), "L").set("hour", hoursArr[i]).toString());
-var hourTask = $("<div>").addClass("hour-task col-lg-9 bg-success");
+var hourDisplay = $("<div>").addClass("hour-display col-lg-1 bg-light")
+    .text(moment()
+        .set("hour", hoursArr[i])
+        .format("hh A").toString());
+var hourTask = $("<div>").addClass("hour-task col-lg-9 bg-success")
+        .attr("hour-id", hoursArr[i]);
 var hourSave = $("<div>").addClass("hour-save col-lg-1 bg-info");
 hourBlockContainer.append(hourDisplay, hourTask, hourSave);
 $("#time-blocks-list").append(hourBlockContainer);
 
-console.log(i);
-console.log(moment(moment(), "L").set("hour", hoursArr[i]).toString());
+if(currentHour > hourTask.attr("hour-id")){
+    hourTask.addClass("bg-danger")
+}
+
+console.log(currentHour + " current hour")
+// console.log(hourByBlock + " hour by block")
 };
 
-
-
-// console.log(moment(new Date(), DD-MM-YYYY))
+//refresh page every 30min
