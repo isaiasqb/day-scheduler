218abb2fe17093e3c677c6a44d92e5b826717ef4
IsaiasQB <isaias.qb@hotmail.com>
2022-03-25 14:11:08 -0400

    comented out some parts to try reverse ingenering the code so far

--- assets/javascript.js
+++ assets/javascript.js
@@ -3,6 +3,7 @@
 // create an array with times times for the working day
 var hoursArr = ["7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19"]
 
+
 // display the local time in the header
 $("#currentDay").text(moment().format("LL").toString())
 
@@ -11,60 +12,67 @@ $("#currentDay").text(moment().format("LL").toString())
 var currentHour = moment().format("H").toString() // this is the current hour (ex. 02)
 
 
-
-
 //TIME BLOCKS dinamically created 
-for(var i = 0; i < hoursArr.length; i++){
+// var initializePage = function (){
+    // var savedTasks = readLocalStorage()
 
-    // create the row that contains the task sections
-    var hourBlockContainer = $("<div>").addClass("time-block row")
-            .attr("hour-id", hoursArr[i]);
-    
+    for(var i = 0; i < hoursArr.length; i++){
+        //iterator for each hour of the day
+        var hourId = hoursArr[i];
+        // create the row that contains the task sections
+        var hourBlockContainer = $("<div>").addClass("time-block row")
+                .attr("hour-id", hourId);
+        
             //create the display of each hour of the day
-    var hourDisplay = $("<div>").addClass("hour-display col-lg-1 bg-light")
-    var blockHour = (moment().set("hour", hoursArr[i]).format("hh A"));
-    hourDisplay.append(blockHour)
-    
-    //create the description text area for heach hourly task
-    var hourTask = $("<p>").addClass("hour-task col-lg-9")
-            .attr("hour-id", hoursArr[i])
-    
-    // create the save button area
-    var saveIcon = $("<i>").addClass("fa-solid fa-floppy-disk"); 
-    var hourSave = $("<div>").addClass("saveBtn col-lg-1 bg-info")
-            .attr("hour-id", hoursArr[i])
-            .append(saveIcon);
-    
+        var hourDisplay = $("<div>").addClass("hour-display col-lg-1 bg-light")
+        var blockHour = (moment().set("hour", hourId).format("hh A"));
+        hourDisplay.append(blockHour)
+        
+            //create the description text area for heach hourly task
+        var hourTask = $("<textarea>").addClass("textarea description col-lg-9")
+                .attr("hour-id", hourId)
+                // .val(savedTasks[hourId]); //assigns the value of the local storage inofmration
+        
+            // create the save button area
+        var saveIcon = $("<i>").addClass("fa-solid fa-floppy-disk"); 
+        var hourSave = $("<div>").addClass("saveBtn col-lg-1 bg-info")
+                .attr("hour-id", hourId)
+                .append(saveIcon);
+        
             //append al all 3 sections to the parents
-    hourBlockContainer.append(hourDisplay, hourTask, hourSave);
-    $("#time-blocks-list").append(hourBlockContainer);
+        hourBlockContainer.append(hourDisplay, hourTask, hourSave);
+        $("#time-blocks-list").append(hourBlockContainer);
+        
+            //dynamically change the color depending on the hour
+        if(Math.abs(currentHour) > Math.abs(hourTask.attr("hour-id"))){
+            hourTask.addClass("past")
+        }  else if (Math.abs(currentHour) === Math.abs(hourTask.attr("hour-id"))){
+            hourTask.addClass("present")}
+        else{
+            hourTask.addClass('future')
+        };
+    }; //...end of for loop
+// }
 
-    
-    //dynamically change the color depending on the hour
-    if(Math.abs(currentHour) > Math.abs(hourTask.attr("hour-id"))){
-        hourTask.addClass("past")
-    }  else if (Math.abs(currentHour) === Math.abs(hourTask.attr("hour-id"))){
-        hourTask.addClass("present")}
-    else{
-        hourTask.addClass('future')
-    };
-}; //...end of for loop
+
+
 
 
 
 //CHANGE descriptive text for every task
-$(".hour-task").on("click", function() {
+$('.saveBtn').on("click", function() {
     var text = $(this) //selects the text value of the element clicked on
-      .text()
-      .trim();
-  
+    .text()
+    .trim();
+
     var textInput = $("<textarea>").addClass("col-lg-9")
-      .addClass("textarea")
-      .val(text); // creates a nex textarea and gives  passes the p text as the text value of this new text area
-  
+    .addClass("textarea")
+    .val(text); // creates a nex textarea and gives  passes the p text as the text value of this new text area
+
     $(this).replaceWith(textInput);  //replaced the <p> element with the <textarea>
-  
+
     textInput.trigger("focus"); //places cursor on text area
+    console.log("you clikced save")
   });
 
 
@@ -82,7 +90,6 @@ $(".time-block").on("blur", "textarea", function(){
 
     // convert the <textarea> back into a <p> element. 
     var description = $("<p>")
-    //   .addClass("m-1")
       .attr("hour-id", id)
       .text(text)
       .addClass("col-lg-9 description")
@@ -99,23 +106,44 @@ $(".time-block").on("blur", "textarea", function(){
 
 
 
-var savedTasks = {
+
+
+
+
+var defaultSavedTasks = {
     7:"",8:"",9:"",10:"",11:"",12:"",13:"",14:"",15:"",16:"",17:"",18:"",19:""
 }
 
 //save in local storage
 var saveToLocal = function(id, data){
     // console.log(id, data)
+    var savedTasks = readLocalStorage()
     savedTasks[id] = data
 
     localStorage.setItem("savedTasks", JSON.stringify(savedTasks));
 };
 
-    var parsedSavedTasks = JSON.parse(localStorage.getItem("savedTasks"))
-    console.log(parsedSavedTasks)  
+var readLocalStorage = function(){ 
+    //access local storage
+    var savedTasks = JSON.parse(localStorage.getItem("savedTasks"));
+    //check if previous data exists
+    //if no porevious data, initialize to default object
+    if(!savedTasks){
+        savedTasks = defaultSavedTasks}
+
+    //return our object
+    return savedTasks
+};
+
+// initializePage()
 
 
 
+
+
+//we want to read all of out text areas
+//loop through each one
+//
 
 
 
